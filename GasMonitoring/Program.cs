573f68e8376492ba8d62f973b6e3974c53505164
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-30 12:24:17 +0100

    Initial commit

---
+++ GasMonitoring/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GasMonitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

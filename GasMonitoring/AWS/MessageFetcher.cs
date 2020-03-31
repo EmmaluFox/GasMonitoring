e77b32cc14aa63abca2a29732809ded972e1c416
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-31 16:26:31 +0100

    Working

---
+++ GasMonitoring/AWS/MessageFetcher.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Newtonsoft.Json;
+
+namespace GasMonitoring.AWS
+{
+    public class MessageFetcher
+    {
+        private readonly AmazonSQSClient _sqsClient = new AmazonSQSClient(RegionEndpoint.EUWest2);
+        private readonly AmazonSimpleNotificationServiceClient _snsClient = new AmazonSimpleNotificationServiceClient();
+        private readonly string _topicArn = "arn:aws:sns:eu-west-2:099421490492:GasMonitoring-snsTopicSensorDataPart1-1YOM46HA51FB";
+        private readonly CreateQueueRequest _createQueueRequest = new CreateQueueRequest("Gas-Monitor-Queue");
+        
+        public MessageFetcher(AmazonSQSClient sqsClient, AmazonSimpleNotificationServiceClient snsClient,
+            string topicArn, CreateQueueRequest createQueueRequest)
+        {
+            this._sqsClient = sqsClient;
+            this._snsClient = snsClient;
+            this._topicArn = topicArn;
+            this._createQueueRequest = createQueueRequest;
+
+        }
+
+        public async Task<IEnumerable<Message>> FetchMessages(AmazonSQSClient sqsClient, AmazonSimpleNotificationServiceClient snsClient, string topicArn, CreateQueueRequest createQueueRequest)
+        {
+            var queueUrl = sqsClient.CreateQueueAsync(createQueueRequest.QueueName).Result.QueueUrl;
+            await snsClient.SubscribeQueueAsync(topicArn, sqsClient, queueUrl);
+
+            var messageTask = sqsClient.ReceiveMessageAsync(new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrl,
+                WaitTimeSeconds = 30
+            });
+            
+            using var streamReader = new StreamReader(messageTask.Result.ToString());
+            var content = streamReader.ReadToEnd();
+            var messages = JsonConvert.DeserializeObject<List<Message>>(content);
+            return messages;
+        }
+        
+        
+    }
+}
--- GasMonitoring/Program.cs
+++ GasMonitoring/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
+using Amazon;
 using Amazon.S3;
-
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 using GasMonitoring.AWS;
 
 
@@ -10,16 +12,33 @@ namespace GasMonitoring
 {
     class Program
     {
+        
         static async Task Main(string[] args)
         {
-            var s3Client = new AmazonS3Client();
-            var locationsFetcher = new LocationsFetcher(s3Client);
-            var locationsTask = await locationsFetcher.FetchLocations();
-            foreach (var location in locationsTask)
-            {
-                Console.Write(location.Id, location.X, location.Y);
-            }
-           
+        var sqsClient = new AmazonSQSClient(RegionEndpoint.EUWest2);
+        var snsClient = new AmazonSimpleNotificationServiceClient();
+        var s3Client = new AmazonS3Client();
+        var createQueueRequest = new CreateQueueRequest("Gas-Monitor-Queue");
+            
+        var locationsFetcher = new LocationsFetcher(s3Client);
+        
+        var locationsTask = await locationsFetcher.FetchLocations();
+      
+        foreach (var location in locationsTask)
+        {
+            Console.Write(location.Id, location.X, location.Y);
+        }
+            
+        const string snsArn = "arn:aws:sns:eu-west-2:099421490492:GasMonitoring-snsTopicSensorDataPart1-1YOM46HA51FB";
+            
+        var messageFetcher = new MessageFetcher(sqsClient, snsClient, snsArn, createQueueRequest);
+        var messageTask = await messageFetcher.FetchMessages(sqsClient, snsClient, snsArn, createQueueRequest);
+        foreach (var message in messageTask)
+        {
+            Console.Write(message.Body);
+            
+        }
+
         }
      
     }

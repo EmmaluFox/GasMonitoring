fc4d9c4ebf9de369b179725fee8f9c1ec59a6189
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-01 11:48:59 +0100

    Latest

---
+++ GasMonitoring/AWS/SetUpConnections.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+using Amazon;
+using Amazon.Runtime.Internal;
+using Amazon.S3;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace GasMonitoring.AWS
+{
+    public class SetUpConnections
+    {
+        public AmazonSQSClient SqsClient { get; set; }
+        public AmazonSimpleNotificationServiceClient SnsClient { get; set; }
+        public CreateQueueRequest CreateQueueRequest { get; set; }
+        public IAmazonS3 S3Client { get; set; }
+        
+        public SetUpConnections()
+        {
+        this.SqsClient = new AmazonSQSClient();
+        this.SnsClient = new AmazonSimpleNotificationServiceClient();
+        this.CreateQueueRequest = new CreateQueueRequest("Gas-Monitor-Queue");
+        this.S3Client = new AmazonS3Client();
+        }
+
+       
+
+        
+      
+    }
+}
---
+++ GasMonitoring/AWS/SetUpCredentials.cs
@@ -0,0 +1,12 @@
+﻿namespace GasMonitoring.AWS
+{
+    public class SetUpCredentials
+    {
+        public class Credentials
+        {
+            public string BucketName { get; set; }
+            public string FileName { get; set; }
+            public string TopicArn { get; set; }
+        }
+    }
+}

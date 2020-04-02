5b0a8e09e8174fa9e955cfd9451df87e4838870d
Emmalu Fox <emmalu.fox@softwire.com>
2020-04-02 13:19:44 +0100

    Testing

--- GasMon.Tests/UnitTest1.cs
+++ GasMon.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using GasMonitoring.AWS;
 using NUnit.Framework;
 
 namespace GasMon.Tests
@@ -7,6 +8,8 @@ namespace GasMon.Tests
         [SetUp]
         public void Setup()
         {
+            var setCredentials = new SetUpCredentials.Credentials(){BucketName = "gasmonitoring-locationss3bucket-pgef0qqmgwba", FileName = "locations.json", TopicArn = "arn:aws:sns:eu-west-2:099421490492:GasMonitoring-snsTopicSensorDataPart1-1YOM46HA51FB"};
+            var setUpConnections = new SetUpConnections();
         }
 
         [Test]
